822926e6f78a9ebd6306b954f3fb0ba9d939d36c
Daekyu <kdk0723@naver.com>
2016-06-08 19:21:58 +0900

    여행정보 - DB에 담긴 좌표를 기반으로 지도에 화살표출력 및 클릭하면 하단 테이블에 여행정보 출력(비동기처리로)

--- java/kr/co/serendipity/TravelInfoController.java
+++ java/kr/co/serendipity/TravelInfoController.java
@@ -2,21 +2,46 @@
  * @Class : TravelInfoController
  * @Date : 16.06.07
  * @Author : 강대규
- * @Desc : 여행정보를 보여주는 컨트롤러
+ * @Desc : 여행정보를 출력하기 위한 컨트롤러
  */
 
 package kr.co.serendipity;
 
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import kr.co.serendipity.model.LocalDTO;
+import kr.co.serendipity.model.TravelInfoDAO;
+import kr.co.serendipity.model.TravelInfoDTO;
 
 
 @Controller
-@RequestMapping("/travel_info/*")
+@RequestMapping("/travel_info/")
 public class TravelInfoController {
+	
+	@Autowired
+	private SqlSession sqlsession;
 	
 	@RequestMapping("travel_info.htm")
 	public String travel_info() {
+		
 		return "/travel_info/travel_info";
+	}
+	
+	@RequestMapping("getLocalList.htm")
+	public @ResponseBody List<LocalDTO> getLocalList() {
+		TravelInfoDAO dao = sqlsession.getMapper(TravelInfoDAO.class);
+		return dao.getLocalList();
+	}
+	
+	@RequestMapping("getLocalInfo.htm")
+	public @ResponseBody TravelInfoDTO getLocalInfo(String local_code) {
+		TravelInfoDAO dao = sqlsession.getMapper(TravelInfoDAO.class);
+		return dao.getLocalInfo(local_code);
 	}
 }
--- webapp/WEB-INF/views/mypage/inc/my_withdraw.jsp
+++ webapp/WEB-INF/views/mypage/inc/my_withdraw.jsp
@@ -29,35 +29,7 @@
 		</div>
       </article><!-- .content -->
 	  
-	  <div id="sidebar" class="sidebar col-sm-3 col-md-3">
-		<aside class="widget menu">
-		  <header>
-			<h3 class="title">My Account</h3>
-		  </header>
-		  <nav>
-			<ul>
-			  <li>
-				<a href="#"><i class="fa fa-user item-icon"></i>My Profile</a>
-			  </li>
-			  <li>
-				<a href="#"><i class="fa fa-gears item-icon"></i>Update Profile</a>
-			  </li>
-			  <li>
-				<a href="#"><i class="fa fa-mail-forward item-icon"></i>받은 요청</a>
-			  </li>
-			  <li>
-				<a href="#"><i class="fa fa-mail-reply item-icon"></i>보낸 요청</a>
-			  </li>
-			  <li>
-				<a href="#"><i class="fa fa-bell-o item-icon"></i>Etc</a>
-			  </li>
-			  <li class="active">
-				<a href="#"><i class="fa fa-times item-icon"></i>Withdraw from Serendipity</a>
-			  </li>
-			</ul>
-		  </nav>
-		</aside><!-- .menu-->
-	  </div>
+	  <jsp:include page="my_page_sidebar.jsp"></jsp:include>
     </div>
   </div>
 </section><!-- #main -->

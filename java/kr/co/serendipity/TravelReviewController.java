fd4c228d1db4c7d7ecf9b08b146099953482ab80
leesora <leesr1203@naver.com>
2016-06-17 16:31:52 +0900

    Revert "필터링"
    This reverts commit fb570f0921b8c390b8de89073360ad20edc185ef.

--- java/kr/co/serendipity/TravelReviewController.java
+++ java/kr/co/serendipity/TravelReviewController.java
@@ -428,15 +428,25 @@ public class TravelReviewController {
 	
 	//여행후기 게시판 지역별로 필터링
 	@RequestMapping("filteringReviewList.htm")
-	public @ResponseBody List<HashMap<String,Object>> filteringReviewList(String local_code, Model model) throws ClassNotFoundException, SQLException{
+	public String filteringReviewList(String local_code ,Model model) throws ClassNotFoundException, SQLException{
 		ReviewDAO dao = sqlsession.getMapper(ReviewDAO.class);
-		return dao.filteringReviewList(local_code);
+		
+		List<HashMap<String,Object>> review_list = dao.filteringReviewList(local_code);
+		model.addAttribute("reviewList", review_list);
+		model.addAttribute("local_list", dao.localList());
+		//model.addAttribute("local_code", local_code);
+		return "redirect:/travel_review/review_list.htm";
 	}
 	
 	//여행후기 게시판 리스트 정렬
 	@RequestMapping("orderReviewList.htm")
-	public @ResponseBody List<HashMap<String,Object>> orderReviewList(String order, Model model) throws ClassNotFoundException, SQLException{
+	public String orderReviewList(String order, Model model) throws ClassNotFoundException, SQLException{
+
 		ReviewDAO dao = sqlsession.getMapper(ReviewDAO.class);
-		return dao.orderReviewList(order);
+
+		List<HashMap<String,Object>> review_list = dao.orderReviewList(order);
+		model.addAttribute("reviewList", review_list);
+		model.addAttribute("local_list", dao.localList());
+		return "redirect:/travel_review/review_list.htm";
 	}
 }
